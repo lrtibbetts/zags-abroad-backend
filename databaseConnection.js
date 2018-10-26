7cd8a8da637a4ee995a1a9292e6a24cf437c0676
Kristen Burgett <38116224+kburgett@users.noreply.github.com>
2018-10-26 16:26:18 -0700

    Update databaseConnection.js

--- databaseConnection.js
+++ databaseConnection.js
@@ -22,7 +22,7 @@ app.post('/signup', function(req, resp) {
   var first = req.first_name;
   var last = req.last_name;
   var password = req.password;
-  var admin = req.is_admin;
+  var admin = 0;
   //var query = "INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES ('cnorman2@zagmail.gonzaga.edu', 'Claire', 'Norman', 'pass1234', 0)"
   //"INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)" , email, first, last, password, admin,
   connection.query("INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)", email, first, last, password, admin, function(error, results, fields) {
