2b3afb7e99e06aa0047c97f783635edbc15a74ff
kburgett <kburgett@zagmail.gonzaga.edu>
2018-10-26 17:41:28 -0700

    Connection testing with Lucy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mySQL = require('mysql');
 var app = express();
 
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 var connection = mySQL.createConnection({
   host: 'us-cdbr-iron-east-01.cleardb.net',
   user: 'bb1f6f893c4a2e',
@@ -22,15 +22,16 @@ app.get('/', function(req, res) {
     res.send('Hello');
 });
 
-app.get('/signup', function(req, res) {
-  var email = "Lucy@gmail.com"//req.email;
-  var first = "Lucy" //req.first_name;
-  var last = "Tibbetts" //req.last_name;
-  var password = "TimBits"//req.password;
+app.post('/signup', function(req, res) {
+  var email = req.email;
+  var first = req.first_name;
+  var last = req.last_name;
+  var password = req.password;
   var admin = 0;
+  res.send(email);
   //var query = "INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES ('cnorman2@zagmail.gonzaga.edu', 'Claire', 'Norman', 'pass1234', 0)"
   //"INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)" , email, first, last, password, admin,
-  connection.query("INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)", email, first, last, password, admin, function(error, results, fields) {
+  connection.query("INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)", [email, first, last, password, admin], function(error, results, fields) {
     if(error) {
       console.log("Error inserting data");
     } else {
