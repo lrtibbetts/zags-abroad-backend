732fb3b37b5023037943c9ab68629c136eccba12
kburgett <kburgett@zagmail.gonzaga.edu>
2018-10-26 19:10:44 -0700

    Check stringify for passing in information to sign up page

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-//declaring the dependencies that we will use to run the post and get requests
+//Declaring dependencies that will be used to run 'post' and 'get' requests
 var express = require('express');
 var mySQL = require('mysql');
 var cors = require('cors');
 
-//this declares that we are using an express object
+//Declares that we are using an express object
 var app = express();
 app.use(cors());
 
-//our port will be determined by heroku, or else it will be set as default to 3000
+//Port determined by Heroku,else Default as localhost:3000
 var port = process.env.PORT || 3000;
 
-//setting up params for a db connection to the sql database
-//we are using cleardb to maintain a database connection through our heroku server
+//Setting up params for DB connection to the SQL database
+//Using ClearDB to maintain database connection through Heroku server
 var connection = mySQL.createConnection({
   host: 'us-cdbr-iron-east-01.cleardb.net',
   user: 'bb1f6f893c4a2e',
@@ -19,7 +19,7 @@ var connection = mySQL.createConnection({
   database: 'heroku_6d22e6ede03be83'
 });
 
-//declares the actual connection to the database
+//Declares the connection to database
 connection.connect(function(error) {
   if(error) {
     console.log('Error');
@@ -28,16 +28,15 @@ connection.connect(function(error) {
   }
 });
 
-//this leads to the sign up page from the frontend
-//this post will be retrieving data from the user as they input their information
-//we will then retreive this information
-//we will use this data to query our db
-//if the use doesn't exist, then we will insert into the database
+//SIGN UP PAGE
+//Recieves data from user inputed information
+//Querys db using this data
+//If the use does not exist, then insert into the database
 app.post('/signup', function(req, res) {
-  var email = req.email;
-  var first = req.first;
-  var last = req.last;
-  var password = req.password;
+  var email = JSON.stringify(req.email);
+  var first = JSON.stringify(req.first);
+  var last = JSON.stringify(req.last);
+  var password = JSON.stringify(req.password);
   var admin = 0;
   connection.query("INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)", [email, first, last, password, admin], function(error, results, fields) {
     if(error) {
@@ -48,8 +47,8 @@ app.post('/signup', function(req, res) {
   });
 });
 
-//this leads to the login page on the frontend
-//this get request checks the current database.
+//LOGIN PAGE
+//Checks the current database
 //if the current user does not exist in the db, then it will return false
 //this takes into account if the email is spelled incorrectly
 app.get('/login', function(req, res) {
