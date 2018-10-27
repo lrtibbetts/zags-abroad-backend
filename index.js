76211bf1e7908a633a83fcfeec98f7d7911381f1
kburgett <kburgett@zagmail.gonzaga.edu>
2018-10-26 17:00:55 -0700

    Trying to make connection

--- index.js
+++ index.js
@@ -17,12 +17,16 @@ connection.connect(function(error) {
   }
 });
 
+app.get('/', function(req, resp) {
+    resp.send('Hello');
+});
+
 app.post('/signup', function(req, resp) {
   var email = req.email;
   var first = req.first_name;
   var last = req.last_name;
   var password = req.password;
-  var admin = req.is_admin;
+  var admin = 0;
   //var query = "INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES ('cnorman2@zagmail.gonzaga.edu', 'Claire', 'Norman', 'pass1234', 0)"
   //"INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)" , email, first, last, password, admin,
   connection.query("INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)", email, first, last, password, admin, function(error, results, fields) {
