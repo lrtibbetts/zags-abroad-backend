b26a3388124ac4258b59c9b1eee5a3839d01eec0
kburgett <kburgett@zagmail.gonzaga.edu>
2018-10-26 17:30:47 -0700

    Heroku maybe?

--- server.js
+++ server.js
@@ -3,41 +3,41 @@ var mySQL = require('mysql');
 var app = express();
 
 var port = process.env.PORT;
-// var connection = mySQL.createConnection({
-//   host: 'us-cdbr-iron-east-01.cleardb.net',
-//   user: 'bb1f6f893c4a2e',
-//   password: '12aba883',
-//   database: 'heroku_6d22e6ede03be83'
-// });
-//
-// connection.connect(function(error) {
-//   if(error) {
-//     console.log('Error');
-//   } else {
-//     console.log('Connection');
-//   }
-// });
+var connection = mySQL.createConnection({
+  host: 'us-cdbr-iron-east-01.cleardb.net',
+  user: 'bb1f6f893c4a2e',
+  password: '12aba883',
+  database: 'heroku_6d22e6ede03be83'
+});
+
+connection.connect(function(error) {
+  if(error) {
+    console.log('Error');
+  } else {
+    console.log('Connection');
+  }
+});
 
 app.get('/', function(req, res) {
     res.send('Hello');
 });
 
-// app.post('/signup', function(req, res) {
-//   var email = req.email;
-//   var first = req.first_name;
-//   var last = req.last_name;
-//   var password = req.password;
-//   var admin = 0;
-//   //var query = "INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES ('cnorman2@zagmail.gonzaga.edu', 'Claire', 'Norman', 'pass1234', 0)"
-//   //"INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)" , email, first, last, password, admin,
-//   connection.query("INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)", email, first, last, password, admin, function(error, results, fields) {
-//     if(error) {
-//       console.log("Error inserting data");
-//     } else {
-//       console.log("Successful insert");
-//     }
-//   });
-// });
+app.get('/signup', function(req, res) {
+  var email = "Lucy@gmail.com"//req.email;
+  var first = "Lucy" //req.first_name;
+  var last = "Tibbetts" //req.last_name;
+  var password = "TimBits"//req.password;
+  var admin = 0;
+  //var query = "INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES ('cnorman2@zagmail.gonzaga.edu', 'Claire', 'Norman', 'pass1234', 0)"
+  //"INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)" , email, first, last, password, admin,
+  connection.query("INSERT INTO accounts (email, first_name, last_name, password, is_admin) VALUES (?,?,?,?,?)", email, first, last, password, admin, function(error, results, fields) {
+    if(error) {
+      console.log("Error inserting data");
+    } else {
+      console.log("Successful insert");
+    }
+  });
+});
 
 app.listen(port, function() {
     console.log('Hit it');
