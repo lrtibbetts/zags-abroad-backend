b618bae9d97b10b126b0cfa4c67e94769efb8955
kburgett <kburgett@zagmail.gonzaga.edu>
2019-01-23 16:10:46 -0800

    hold id in account page

--- routes/saved_courses.js
+++ routes/saved_courses.js
@@ -20,7 +20,7 @@ module.exports = {
   //MY ACCOUNT Courses
   accountCourses(req, res) {
     var email = req.body.email;
-    pool.query("SELECT c.host_program, c.host_course_number, c.host_course_name, c.gu_course_number, c.gu_course_name, c.signature_needed " +
+    pool.query("SELECT c.id, c.host_program, c.host_course_number, c.host_course_name, c.gu_course_number, c.gu_course_name, c.signature_needed " +
     "FROM course_equivalencies c JOIN saved_courses s WHERE s.email = ? AND c.id = s.course_id ORDER BY c.host_program ASC", [email],
       function(accError, accResult) {
         if(accError) {
