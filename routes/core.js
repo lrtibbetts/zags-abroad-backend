a780de60c6fa71296d66f2b7ed561b6a8b77d82d
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-02-04 14:27:49 -0800

    program core api

--- routes/core.js
+++ routes/core.js
@@ -11,5 +11,17 @@ module.exports = {
         res.send(coreResult);
       }
     });
+  },
+
+  programCore(req, res) {
+    pool.query("SELECT DISTINCT c.core_name FROM core_designations c JOIN course_equivalencies e" +
+    " WHERE e.core LIKE CONCAT('%', c.core_name, ',%')", function(coreError, coreResult) {
+      if(coreError) {
+        console.log("Cannot get core designations");
+        res.send(coreError);
+      } else {
+        res.send(coreResult);
+      }
+    });
   }
 };
