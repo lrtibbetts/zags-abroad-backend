1defa5ae922dbb96b8172f060bced84004b26c01
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-02-04 14:42:14 -0800

    add program name woops

--- routes/core.js
+++ routes/core.js
@@ -14,13 +14,13 @@ module.exports = {
   },
 
   programCore(req, res) {
+    var program = req.body.program;
     pool.query("SELECT DISTINCT c.core_name FROM core_designations c JOIN course_equivalencies e" +
-    " WHERE e.core LIKE CONCAT('%', c.core_name, ',%')", function(coreError, coreResult) {
-      if(coreError) {
-        console.log("Cannot get core designations");
-        res.send(coreError);
+    " WHERE e.host_program = ? AND e.core LIKE CONCAT('%', c.core_name, ',%')", [program], function(err, result) {
+      if(err) {
+        res.send(err);
       } else {
-        res.send(coreResult);
+        res.send(result);
       }
     });
   }
