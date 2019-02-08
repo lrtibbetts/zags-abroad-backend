19c2892f82e1ebd7b2fcece0444a30d654c1e040
Lucille R. Tibbetts <lucilletibbetts@gmail.com>
2019-02-07 16:46:43 -0800

    store height and width of photos

--- routes/photos.js
+++ routes/photos.js
@@ -3,17 +3,19 @@ var pool = require('../pool.js');
 module.exports = {
 
   submitPhotos(req, res) {
-    var program = req.body.program
-    var photos = req.body.photos
+    var program = req.body.program;
+    var url = req.body.url;
+    var height = req.body.height;
+    var width = req.body.width;
 
-    pool.query("INSERT INTO photos (program, url) VALUES (?,?)",
-    [program, photos],
+    pool.query("INSERT INTO photos (program, url, height, width) VALUES (?,?,?,?)",
+    [program, url, height, width],
     function(error, result) {
       if(error) {
         console.log(error);
         res.send(error);
       } else {
-        console.log("Your pictures are entered into the database")
+        console.log("Your pictures have been entered into the database")
         res.send(result);
       }
     })
@@ -21,7 +23,7 @@ module.exports = {
 
   getProgramPhotos(req, res) {
     var program = req.body.program
-    pool.query("SELECT url FROM photos WHERE program = ?", [program], function(error, result) {
+    pool.query("SELECT url, height, width FROM photos WHERE program = ?", [program], function(error, result) {
       if (error) {
         console.log(error)
         res.send(error)
