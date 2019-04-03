c7f8f398721793fdae5613249a3c7c92c2585bfb
kburgett <kburgett@zagmail.gonzaga.edu>
2019-04-03 14:37:53 -0700

    Core designations ordered ASC

--- routes/core.js
+++ routes/core.js
@@ -3,7 +3,7 @@ var pool = require('../pool.js');
 module.exports = {
   //GET ALL CORE DESIGNATIONS
   getCore(req, res) {
-    pool.query("SELECT * FROM core_designations", function(coreError, coreResult) {
+    pool.query("SELECT * FROM core_designations ORDER BY core_name ASC", function(coreError, coreResult) {
       if(coreError) {
         console.log("Cannot get core designations");
         res.send(coreError);
@@ -20,7 +20,7 @@ module.exports = {
   programCore(req, res) {
     var program = req.body.program;
     pool.query("SELECT DISTINCT c.core_name FROM core_designations c JOIN course_equivalencies e" +
-    " WHERE e.host_program = ? AND e.core LIKE CONCAT('%', c.core_name, ',%')", [program], function(err, result) {
+    " WHERE e.host_program = ? AND e.core LIKE CONCAT('%', c.core_name, ',%') ORDER BY c.core_name ASC", [program], function(err, result) {
       if(err) {
         res.send(err);
       } else {
